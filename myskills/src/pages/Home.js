52979bee141347da99d99183dd6d9af983ec611f
jefflennon1 <jeffersonlennon1@outlook.com>
2021-07-18 15:48:31 -0300

    Style baseado na paltaforma

--- myskills/src/pages/Home.js
+++ myskills/src/pages/Home.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View, 
+  Text, 
+  StyleSheet,
+  TextInput,
+  Platform
+} from 'react-native';
 
 
 export  function Home(){
   return(
     <>
-      <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-      >
-        <Text>Welcome to  React native</Text>
-       <Text > Ignite </Text>
-     </View>
+      <View style={styles.container} >
+        <Text style={styles.title}>Welcome to  React native</Text>
+        <TextInput
+        style={styles.input}
+        />
+      </View>
   </>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal : 20,
+    paddingVertical: 70,
+    backgroundColor: '#101215'
+  },
+  title:{
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input:{
+    backgroundColor: '#1F1E25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 5,
+  }
+});
